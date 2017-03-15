ea6aa5c8b6024acccb485621157d77d4b6c78bec
Dylan Maccora <maccora17@gmail.com>
2017-03-16 08:44:46 +1100

    Day 2 of WeekOfCode30

---
+++ src/main/weekOfCode30/FindMinimumNumber.java
@@ -0,0 +1,29 @@
+
+/**
+ * FindMinimumNumber
+ */
+import java.util.Scanner;
+
+public class FindMinimumNumber {
+
+    static StringBuilder builder = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        createFunctionCall(n);
+        System.out.println(builder.toString());
+    }
+
+    private static String createFunctionCall(int numInput) {
+        // Best way to do this is by recursion
+        if (numInput == 2) {
+            builder.append("min(int, int)");
+        } else {
+            builder.append("min(int, ");
+            createFunctionCall(numInput - 1);
+        }
+
+    }
+
+}
